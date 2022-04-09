d711e80f30e1a806e2143c37c7aad76d52b3e4d2
Alexandre Bueno <alexandre.sbueno@ambevtech.com.br>
2022-04-09 19:15:41 -0300

    Criação das rotas

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,11 +2,13 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { CadastroComponent } from './components/cadastro/cadastro.component';
 import { LoginComponent } from './components/login/login.component';
+import { ProdutoListagemComponent } from './components/produto/produto-listagem/produto-listagem.component';
 
 const routes: Routes = [
-  { path: '', redirectTo: '/shop', pathMatch: 'full' },
+  { path: '', redirectTo: '/home', pathMatch: 'full' },
   { path: 'login', component: LoginComponent },
-  { path: 'cadastrar', component: CadastroComponent }
+  { path: 'cadastrar', component: CadastroComponent },
+  { path: 'home', component: ProdutoListagemComponent },
 ];
 
 @NgModule({
