d791606008d9edb97d2631f21b2923efd5cb8165
Alexandre Bueno <alexandre.sbueno@ambevtech.com.br>
2022-04-13 18:13:58 -0300

    Criação do componente de página não encontrada

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,12 +3,18 @@ import { RouterModule, Routes } from '@angular/router';
 import { CadastroComponent } from './components/cadastro/cadastro.component';
 import { LoginComponent } from './components/login/login.component';
 import { ProdutoListagemComponent } from './components/produto/produto-listagem/produto-listagem.component';
+import { PromocoesComponent } from './components/promocoes/promocoes.component';
+import { PaginaNaoEncontradaComponent } from './components/shared/pagina-nao-encontrada/pagina-nao-encontrada.component';
+import { SobreComponent } from './components/sobre/sobre.component';
 
 const routes: Routes = [
   { path: '', redirectTo: '/home', pathMatch: 'full' },
   { path: 'login', component: LoginComponent },
   { path: 'cadastrar', component: CadastroComponent },
   { path: 'home', component: ProdutoListagemComponent },
+  { path: 'promocoes', component: PromocoesComponent },
+  { path: 'sobre', component: SobreComponent },
+  { path: '**', component: PaginaNaoEncontradaComponent }
 ];
 
 @NgModule({
